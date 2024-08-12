1c0e359c4189716c67d95583326278300199adaa
Noah Meletis <176389336+noahmeletis@users.noreply.github.com>
2024-08-12 11:09:36 +1000

    Added all of the move set arrays for 001-020

---
+++ script.js
@@ -0,0 +1,67 @@
+//001-003
+const bulbasaurMovesStr = 'Tackle Growl Vine-Whip Growth Leech-Seed Razor-Leaf Poison-Powder Sleep-Powder Seed-Bomb Take-Down Sweet-Scent Synthesis Worry-Seed Power-Whip Solar-Beam';
+const bulbasaurMovesArr = bulbasaurMovesStr.split(' ');
+
+const ivysaurMovesArr = bulbasaurMovesArr;
+
+const venusaurMovesStr = 'Petal-Dance Tackle Growl Vine-Whip Growth Petal-Blizzard Leech-Seed Razor-Leaf Poison-Powder Sleep-Powder Seed-Bomb Take-Down Sweet-Scent synthesis Worry-Seed Power-Whip Solar-Beam';
+const venusaurMovesArr = venusaurMovesStr.split(' ');
+
+//004-006
+const charmanderMovesStr = 'Scratch Growl Ember Smokescreen Dragon-Breath Fire-Fang Slash Flamethrower Scary-Face Fire-Spin Inferno Flare-Blitz';
+const charmanderMovesArr = charmanderMovesStr.split(' ');
+
+const charmeleonMovesStr = 'Scratch Growl Ember Smokescreen Drago-Breath Fire-Fang Slash Flamethrower Scary-Face Inferno Flare-Blitz';
+const charmeleonMovesArr = charmeleonMovesStr.split(' ');
+
+const charizardMovesStr = 'Dragon-Claw Heat-Wave Scratch Growl Ember Smokescreen Air-Slash Dragon-Breath Fire-Fang Slash Flamethrower Scary-Face Fire-Spin Inferno Flare-Blitz';
+const charizardMovesArr = charizardMovesStr.split(' ');
+
+//007-009
+
+const squirtleMovesStr = 'Tackle Tail-Whip Water-Gun Withdraw Rapid-Spin Bite Water-Pulse Protect Rain-Dance Aqua-Tail Shll-Smash Iron-Defense Hydro-Pump Wave-Crash';
+const squirtleMovesArr = squirtleMovesStr.split(' ');
+
+const wartortleMovesArr = squirtleMovesArr;
+
+const blastoiseMovesStr = 'Tackle Tail-Whip Water-Gun Withdraw Flash-Cannon Rapid-Spin Bite Water-Pulse Protect Rain-Dance Aqua-Tail Shell-Smash Iron-Defence Hydro-Pump Wave-Crash';
+const blastoiseMovesArr = blastoiseMovesStr.split(' ');
+
+//010-012
+
+const caterpieMovesStr = 'Tackle String-Shot Bug-Bite';
+const caterpieMovesArr = caterpieMovesStr.split(' ');
+
+const metapodMovesStr = 'Tackle String-Shot Bug-Bite Harden';
+const metapodMovesArr = metapodMovesStr.split(' ');
+
+const butterfreeMovesStr = 'Gust Harden Tackle String-Shot Bug-Bite Supersonic Confusion Poison-Powder Stun-Spore Psybeam Whirlwind Air-Slash Safeguard Bug-Buzz Tailwind Rage-Powder Quiver-Dance';
+const butterfreeMovesArr = butterfreeMovesStr.split(' ');
+
+//013-015
+
+const weedleMovesStr = 'Poison-Sting String-Shot';
+const weedleMovesArr = weedleMovesStr.split(' ');
+
+const kakunaMovesStr = 'Poison-Sting String-Shot Harden';
+const kakunaMovesArr = kakunaMovesStr.split(' ');
+
+const beedrillMovesStr = 'Poison-Sting String-Shot Harden Fury-Attack Focus-Energy Twineedle Rage Pin-Missle Agility';
+
+//016-018
+
+const pidgeyMovesStr = 'Tackle Sand-Attack Gust Quick-Attack Whirlwind Twister Feather-Dance Agility Wing-Attack Roost Tailwind Aerial-Ace Air-Slash Hurricane';
+const pidgeyMovesArr = pidgeyMovesStr.split(' ');
+
+const pidgeottoMovesArr = pidgeyMovesArr;
+
+const pidgeotMovesArr = pidgeottoMovesArr;
+
+//019-020
+
+const rattataMovesStr = 'Tackle Tail-Whip Quick-Attack Focus-Energy Bite Laser-Focus Take-Down Assurance Crunch Sucker-Punch Super-Fang Double-Edge Endeavor';
+const rattataMovesArr = rattataMovesStr.split(' ');
+
+const raticateMovesStr = 'Scary-Face Swords-Dance Tackle Tail-whip Quick-Attack Focus-Energy Bite Laser-Focus Take-Down Assurance Crunch Sucker-Punch Super-Fang Double-Edge Endeavor';
+const raticateMovesArr = raticateMovesStr.split(' ');
+
